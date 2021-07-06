933d1b17e9fd4a27cebdebbf52997e73b70dd99a
Borja Pilarte <pilit63@hotmail.com>
2021-07-06 19:46:41 +0200

    add articles list and new pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,17 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all
+  end
+
+  def new
+
+  end
+
+  def create
+    render plain: params[:article]
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'general_pages#home'
   get 'about', to: 'general_pages#about'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index, :new, :create]
 end
