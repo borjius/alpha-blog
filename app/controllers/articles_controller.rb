882902a29161bef192b5cf964669648fd2a92a13
Borja Pilarte <pilit63@hotmail.com>
2021-07-14 22:36:03 +0200

    Add users in articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       # notice or alert available
       # this is a rails helper
